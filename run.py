2bda60b628499339da8dcb9854b0f71a67b9f086
HichemHadhri <mohamed.hadhri@epfl.ch>
2023-12-07 19:27:46 +0000

    add code  to downoad swissport in run.ipynb

--- run.py
+++ run.py
@@ -9,7 +9,7 @@ from datasets.cell.tabula_muris import *
 from utils.io_utils import get_resume_file, hydra_setup, fix_seed, model_to_dict, opt_to_dict, get_model_file
 
 import debugpy
-debugpy.listen(('localhost', 5678))
+#debugpy.listen(('localhost', 5678))
 
 def initialize_dataset_model(cfg):
     # Instantiate train dataset as specified in dataset config under simple_cls or set_cls
